1a781362de5f7ee8ef43497e5061e3fd6754f27d
liyz44 <liyz44@gdii-yueyun.com>
2020-12-24 14:18:51 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import index from '@/views/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '首页',
+      component: index
+    }
+  ]
+})
---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
